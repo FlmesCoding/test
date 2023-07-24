c47c378e287d55d6c28c0a89f5b6cc05c6436fed
Flames <134011556+FlmesCoding@users.noreply.github.com>
2023-07-23 22:17:31 -0400

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 537413528 then
+loadstring(game:HttpGet('https://pastebin.com/raw/wjfF62S1'))()
+
+if game.PlaceId == 142823291 then
+loadstring(game:HttpGet('https://pastebin.com/raw/CaUfeLUF'))()
+end
